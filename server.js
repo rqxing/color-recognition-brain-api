22e667f272f8d93dc652d0b0a78cb5904831910a
Ruiqi Xing <eva.rq.xing@gmail.com>
2020-03-08 15:20:45 +0800

    REST API: /signin, /register, /profile/:id, /image

--- server.js
+++ server.js
@@ -1,9 +1,105 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const app = express();
+app.use(express.json())
 
+const database = {
+    users:[
+        {
+            id:'0',
+            name:'eva',
+            email:'eva@gmail.com',
+            entries:0,
+            joined:new Date()
+        },
+        {
+            id:'1',
+            name:'shu',
+            email:'shu@gmail.com',
+            entries:0,
+            joined:new Date()
+        }
+    ],
+    login:[
+        {
+            email:"eva@gmail.com",
+            hash:bcrypt.hashSync('eva123', bcrypt.genSaltSync(10))
+        },
+        {
+            email:"shu@gmail.com",
+            hash:bcrypt.hashSync('shu123', bcrypt.genSaltSync(10))
+        }
+    ]
+}
 app.get('/',(req,res)=>{
     res.send('homepage is working');
+})
+
+app.post('/signin',(req,res)=>{
+    const {email,password} = req.body;
+    let found = false;
+    database.login.forEach(user =>{
+        if(user.email === email){
+            found = true;
+            if(bcrypt.compareSync(password, user.hash)){
+                return res.json('successfully logging in');
+            }else{
+                return res.status(400).json('error logging in');
+            }
+        }
+    })
+    if(found===false){
+        res.status(400).json('no such user')
+    }
+})
+
+app.post('/register',(req,res)=>{
+    const {name,email,password} = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const id = database.users.length.toString();
+    database.users.push({
+        id:id, 
+        name:name,
+        email:email,
+        entries:0,
+        joined:new Date()
+    })
+    database.login.push({
+        email:email,
+        hash:hash
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id',(req,res)=>{
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user =>{
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+    })
+    if(found===false){
+        res.status(400).json('no such user')
+    }
+})
+
+app.put('/image',(req,res)=>{
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user =>{
+        if(user.id === id){
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if(found===false){
+        res.status(400).json('no such user')
+    }
 })
 
 app.listen(3001,()=>{
